daad1d798b49228c66eafa30f5eed4246480b847
Ankita <ankita.kumari@tesco.com>
2019-02-12 19:15:42 +0530

    Added firebase, axios and spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -23,6 +25,7 @@ class BurgerBuilder extends Component {
         totalPrice : 4,
         purchasable: false,
         purchasing: false,
+        loading: false
     }
     updatePurchaseState (ingredients) {
         const sum = Object.keys(ingredients).map(igKey => {
@@ -70,9 +73,27 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        //alert('You continue!');
+        this.setState({loading: true})
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Ankita',
+                address: {
+                    street: 'Nineleaps lane',
+                    zipCode: '560034',
+                    country: 'India'
+                },
+                email: 'Test@test.com'   
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json',order)
+        .then(response => this.setState({ loading:false, purchasing: false }))
+        .catch(error => this.setState({ loading:false, purchasing: false }));
     }
-        render () {
+        render () { 
             const disabledInfo = {
                 ...this.state.ingredients
             };
@@ -80,13 +101,17 @@ class BurgerBuilder extends Component {
             for(let key in disabledInfo){
                 disabledInfo[key] = disabledInfo[key] <= 0;
             }
+            let orderSummary =  <OrderSummary ingredients={this.state.ingredients}
+                                              purchaseCancelled={this.purchaseCancelHandler} 
+                                              purchaseContinued={this.purchaseContinueHandler}
+                                              price={this.state.totalPrice} />
+            if(this.state.loading){
+                orderSummary = <Spinner />
+            }
             return (
                 <React.Fragment>
                     <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                        <OrderSummary ingredients={this.state.ingredients}
-                                      purchaseCancelled={this.purchaseCancelHandler} 
-                                      purchaseContinued={this.purchaseContinueHandler}
-                                      price={this.state.totalPrice}/>
+                       {orderSummary}
                     </Modal>
                     <Burger ingredients={this.state.ingredients}/>
                     <BuildControls 
