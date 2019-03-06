c41d06d4cb0047f1fd0e037127c70174ecf4a056
Ankita <ankita.kumari@tesco.com>
2019-03-06 12:22:17 +0530

    Added orders page to /orders navigation link

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,29 @@ import React from 'react';
 import classes from './Order.css'
 
 const order = (props) => {
+    const ingredientOutput = [];
+
+    for(let ingredientName in props.ingredients){
+        ingredientOutput.push({
+            name: ingredientName,
+            quantity: props.ingredients[ingredientName]
+        })
+    }
+    let ingredientsDisplayed = ingredientOutput.map(igKey => {
+        return <span 
+            style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'}}>{igKey.name} ({igKey.quantity})
+            </span>
+    }) 
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad(1)</p>
-            <p>Price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredientsDisplayed}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
         </div>
     )
 };
