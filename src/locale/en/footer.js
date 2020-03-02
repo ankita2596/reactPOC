61759c4234579af11efec106c8935347fce093ed
Ankita <ankita.kumari@tesco.com>
2020-03-02 16:19:26 +0530

    Ankita | Added localisation folder

---
+++ src/locale/en/footer.js
@@ -0,0 +1,17 @@
+export default {
+  "Footer.heretoHelp": "Here to Help",
+  "Footer.site": "Tesco sites",
+  "Footer.usefulLinks": "Useful links",
+  "Footer.email": "Email",
+  "Footer.supplierSupport": "Supplier Support",
+  "Footer.supplierProtectorLine": "Supplier Protector Line",
+  "Footer.supplierSupportHelpline": "Supplier Support Helpline",
+  "Footer.colleagueSupportHelpline": "Colleague Support Helpline",
+  "Footer.termsAndCondition": "Terms and conditions",
+  "Footer.privacyPolicy": "Privacy and cookies policy",
+  "Footer.follow": "Follow us",
+  "Footer.rightsreserved": "All rights reserved",
+  "Footer.ourSite": "Our website",
+  "Footer.docView": "Documents",
+  "Footer.newsView": "News"
+};
---
+++ src/locale/en/header.js
@@ -0,0 +1,15 @@
+export default {
+  "Header.system": "Systems",
+  "Header.document": "Documents",
+  "Header.news": "News",
+  "Header.profile": "My profile",
+  "Header.company": "My company",
+  "Header.manage": "Manage users",
+  "Header.signout": "Sign out",
+  "Header.managedocuments": "Manage documents",
+  "Header.salesreport": "Sales reports",
+  "Header.documentlibrary": "Document library",
+  "Header.systemlinks": "System links",
+  "Header.manageusers": "Manage users",
+  "Header.toolslist": "Tools list"
+};
